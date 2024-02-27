a24b56161984cb397b4bb94d2d6207b4a8fab554
songxingguo <xg.song@qq.com>
2024-02-27 14:48:00 +0800

    feat: three.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
-  return <div></div>;
+  return (
+    <div id="canvas-container">
+      <Canvas height="500">
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[5, 5, 5]} />
+        <mesh>
+          <boxGeometry args={[5, 5, 5]} />
+          <meshStandardMaterial />
+        </mesh>
+        <OrbitControls enableZoom={false} autoRotate />
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
