9f1fe764caf04ecc2bd42883c83770a5819d7154
songxingguo <xg.song@qq.com>
2024-04-15 07:14:06 +0800

    feat: 生成组件

---
+++ src/.vuepress/components/Chat.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div ref="el"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import useHtml from "@hooks/useHtml.js";
+
+const el = ref();
+
+onMounted(async () => {
+  const { html, css, js } = await useHtml("/demo/WebRTC/Chat/Chat.html");
+
+  new MiniSandbox({
+    el: el.value,
+    files: {
+      "index.html": {
+        defaultValue: html,
+        
+        jsLibs: ["index.js"],
+      },
+      
+      "index.js": {
+        defaultValue: js,
+      },
+    },
+    defaultConfig: {
+      height: "500px",
+    },
+  });
+});
+</script>
