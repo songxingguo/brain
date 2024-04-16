e624a9b3d92b715661816999ec38227b4d0b88dd
songxingguo <xg.song@qq.com>
2024-04-16 12:03:23 +0800

    feat: 探索three.js-响应式处理

--- src/.vuepress/public/demo/Three/World/World.js
+++ src/.vuepress/public/demo/Three/World/World.js
@@ -26,6 +26,9 @@ class World {
     scene.add(cube, light);
 
     const resizer = new Resizer(container, camera, renderer);
+    resizer.onResize = () => {
+      this.render();
+    };
   }
 
   // 2. Render the scene
