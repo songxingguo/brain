18e41b7e701c32312ce30eb61be98643ef3b96cf
songxingguo <xg.song@qq.com>
2024-04-16 11:07:22 +0800

    feat: 探索Three.js-three.js 世界模版

---
+++ src/.vuepress/public/demo/Three/World/World.js
@@ -0,0 +1,36 @@
+import { createCamera } from "./components/camera.js";
+import { createCube } from "./components/cube.js";
+import { createScene } from "./components/scene.js";
+
+import { createRenderer } from "./systems/renderer.js";
+import { Resizer } from "./systems/Resizer.js";
+
+// These variables are module-scoped: we cannot access them
+// from outside the module
+let camera;
+let renderer;
+let scene;
+
+class World {
+  // 1. Create an instance of the World app
+  constructor(container) {
+    camera = createCamera();
+    scene = createScene();
+    renderer = createRenderer();
+    container.append(renderer.domElement);
+
+    const cube = createCube();
+
+    scene.add(cube);
+
+    const resizer = new Resizer(container, camera, renderer);
+  }
+
+  // 2. Render the scene
+  render() {
+    // draw a single frame
+    renderer.render(scene, camera);
+  }
+}
+
+export { World };
---
+++ src/.vuepress/public/demo/Three/World/main.js
@@ -0,0 +1,16 @@
+import { World } from "./World.js";
+
+// create the main function
+function main() {
+  // Get a reference to the container element
+  const container = document.querySelector("#scene-container");
+
+  // 1. Create an instance of the World app
+  const world = new World(container);
+
+  // 2. Render the scene
+  world.render();
+}
+
+// call main to start the app
+main();
