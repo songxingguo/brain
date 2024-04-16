7cbd4f6af4402d426caf04cbf1bb3d1c3b4207b4
songxingguo <xg.song@qq.com>
2024-04-16 14:11:17 +0800

    feat: 探索three.js-纹理映射

--- src/.vuepress/public/demo/Three/World/src/components/cube.js
+++ src/.vuepress/public/demo/Three/World/src/components/cube.js
@@ -3,23 +3,39 @@ import {
   MathUtils,
   Mesh,
   MeshStandardMaterial,
+  TextureLoader,
 } from "three";
+
+function createMaterial() {
+  // create a texture loader.
+  const textureLoader = new TextureLoader();
+
+  // load a texture
+  const texture = textureLoader.load("./assets/textures/uv-test-bw.png");
+
+  // create a "standard" material using
+  // the texture we just loaded as a color map
+  const material = new MeshStandardMaterial({
+    map: texture,
+  });
+
+  return material;
+}
 
 function createCube() {
   const geometry = new BoxBufferGeometry(2, 2, 2);
-  const material = new MeshStandardMaterial({ color: "purple" });
+  const material = createMaterial();
   const cube = new Mesh(geometry, material);
 
   cube.rotation.set(-0.5, -0.1, 0.8);
 
   const radiansPerSecond = MathUtils.degToRad(30);
 
-  // this method will be called once per frame
   cube.tick = (delta) => {
     // increase the cube's rotation each frame
-    cube.rotation.z += radiansPerSecond * delta;
-    cube.rotation.x += radiansPerSecond * delta;
-    cube.rotation.y += radiansPerSecond * delta;
+    cube.rotation.z += delta * radiansPerSecond;
+    cube.rotation.x += delta * radiansPerSecond;
+    cube.rotation.y += delta * radiansPerSecond;
   };
 
   return cube;
