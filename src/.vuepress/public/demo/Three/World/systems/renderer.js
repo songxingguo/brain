65f2b6b18af35b7e29ab58892f84338eafe0f695
songxingguo <xg.song@qq.com>
2024-04-16 11:54:38 +0800

    feat: 探索three.js-启用抗锯齿

--- src/.vuepress/public/demo/Three/World/systems/renderer.js
+++ src/.vuepress/public/demo/Three/World/systems/renderer.js
@@ -1,7 +1,9 @@
 import { WebGLRenderer } from "three";
 
 function createRenderer() {
-  const renderer = new WebGLRenderer();
+  const renderer = new WebGLRenderer({ antialias: true });
+
+  renderer.physicallyCorrectLights = true;
 
   return renderer;
 }
