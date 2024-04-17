bf4d8ca4ec42bb7644952697491cb0b951c99f39
songxingguo <xg.song@qq.com>
2024-04-18 07:45:46 +0800

    feat: 意见反馈

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -16,7 +16,19 @@ export default defineUserConfig({
     ["script", { type: "text/javascript", src: "https://unpkg.com/mini-sandbox@0.3.18" }],
     ["script", { type: "text/javascript", src: "https://unpkg.com/mini-sandbox@0.3.18/dist/vue-loader.js" }],
     ["script", { type: "text/javascript", src: "https://unpkg.com/mini-sandbox@0.3.18/dist/react-loader.js" }],
-    ["script", { "data-host-id": "1","data-auto-reg":false, "data-welcome": "你好，很高兴认识你，欢迎👏留您的建议",  src: "https://vocechat.songxingguo.com/widget.js" }],
+    ["script", { 
+        "data-host-id":"1",
+        "data-auto-reg":"true" ,
+        "data-login-token":"" ,
+        "data-theme-color":"#1fe1f9" ,
+        "data-close-width":"48" ,
+        "data-close-height":"48" ,
+        "data-open-width":"380" ,
+        "data-open-height":"680" ,
+        "data-welcome":"你好，很高兴认识你，欢迎👏留您的想法或建议" ,
+        src:"https://vocechat.songxingguo.com/widget.js" ,
+        async: true,
+    }],
   ],
   // 网站信息
   locales: {
