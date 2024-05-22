ca14f41d07a1eafe92ca0813d058523ee624a875
宋兴国 <1328989942@qq.com>
2024-05-22 22:02:23 +0800

    feat: useReact

--- src/.vuepress/hooks/useReact.js
+++ src/.vuepress/hooks/useReact.js
@@ -1,6 +1,18 @@
 import axios from "axios";
+const regScript = /<script type="text\/babel">[\s\S]*?<\/script>/;
+
+function resolveSrcipt(htmlStr) {
+  const js = regScript.exec(htmlStr);
+  if (!js) return;
+  const newJs = js[0]
+    .replace(/<script type="text\/babel">/, "")
+    .replace("</script>", "");
+
+  return newJs;
+}
 
 export default async function (path) {
-  const { data: jsx } = await axios.get(path);
+  const { data } = await axios.get(path);
+  const jsx = resolveSrcipt(data);
   return { jsx };
 }
