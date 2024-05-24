537d14eeae8342730f04e62baefd8fdfcd37daaf
宋兴国 <xg.song@qq.com>
2024-05-24 09:52:09 +0800

    feat: 设置vite构建配置，解决无法在顶级作用域使用await

--- src/.vuepress/plugin/vuepress-plugin-demo/index.js
+++ src/.vuepress/plugin/vuepress-plugin-demo/index.js
@@ -6,7 +6,11 @@ export const DemoPlugin = (options) => {
         console.log("--onInitialized");
       },
       extendsBundlerOptions(bundlerOptions, app) {
-        console.log("bundlerOptions");
+        // 修改 @vuepress/bundler-vite 的配置项
+        if (app.options.bundler.name === '@vuepress/bundler-vite') {
+          bundlerOptions.viteOptions.build.target ??= "es2019"
+        }
+        console.log("bundlerOptions", bundlerOptions);
       },
     };
   };
