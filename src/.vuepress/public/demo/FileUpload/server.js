39de26fceca7980814f99b248df45c795df16697
songxingguo <xg.song@qq.com>
2024-06-10 21:00:12 +0800

    feat: 实现大文件分片上传-合并分片

--- src/.vuepress/public/demo/FileUpload/server.js
+++ src/.vuepress/public/demo/FileUpload/server.js
@@ -2,9 +2,35 @@ import * as http from "http"; //ES 6
 import path from "path";
 import fse from "fs-extra";
 import multiparty from "multiparty";
+const extractExt = (filename) =>
+  filename.slice(filename.lastIndexOf("."), filename.length); // 提取后缀名
 
 const server = http.createServer();
 const UPLOAD_DIR = path.resolve(import.meta.dirname, ".", "target"); // 大文件存储目录
+
+const resolvePost = (req) =>
+  new Promise((resolve) => {
+    let chunk = "";
+    req.on("data", (data) => {
+      chunk += data;
+    });
+    req.on("end", () => {
+      resolve(JSON.parse(chunk));
+    });
+  });
+
+// 合并切片
+const mergeFileChunk = async (filePath, fileHash) => {
+  const chunkDir = `${UPLOAD_DIR}/${fileHash}`;
+  const chunkPaths = await fse.readdir(chunkDir);
+  await fse.writeFile(filePath, "");
+  console.log("filePath", filePath);
+  chunkPaths.forEach((chunkPath) => {
+    fse.appendFileSync(filePath, fse.readFileSync(`${chunkDir}/${chunkPath}`));
+    fse.unlinkSync(`${chunkDir}/${chunkPath}`);
+  });
+  fse.rmdirSync(chunkDir); // 合并后删除保存切片的目录
+};
 
 server.on("request", async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -15,29 +41,49 @@ server.on("request", async (req, res) => {
     return;
   }
 
-  const multipart = new multiparty.Form();
+  if (req.url === "/") {
+    const multipart = new multiparty.Form();
 
-  multipart.parse(req, async (err, fields, files) => {
-    if (err) {
-      return;
-    }
-    const [chunk] = files.file;
-    const [hash] = fields.md5;
-    const [filename] = fields.name;
-    const chunkDir = `${UPLOAD_DIR}/${filename}`;
+    multipart.parse(req, async (err, fields, files) => {
+      if (err) {
+        return;
+      }
+      const [chunk] = files.file;
+      const [hash] = fields.hash;
+      const [fileHash] = fields.fileHash;
+      const chunkDir = `${UPLOAD_DIR}/${fileHash}`;
 
-    console.log("chunkDir", chunkDir);
-    // 切片目录不存在，创建切片目录
-    if (!fse.existsSync(chunkDir)) {
-      await fse.mkdirs(chunkDir);
-    }
+      // 切片目录不存在，创建切片目录
+      if (!fse.existsSync(chunkDir)) {
+        await fse.mkdirs(chunkDir);
+      }
 
-    // fs-extra 专用方法，类似 fs.rename 并且跨平台
-    // fs-extra 的 rename 方法 windows 平台会有权限问题
-    // https://github.com/meteor/meteor/issues/7852#issuecomment-255767835
-    await fse.move(chunk.path, `${chunkDir}/${hash}`);
-    res.end("received file chunk");
-  });
+      // fs-extra 专用方法，类似 fs.rename 并且跨平台
+      // fs-extra 的 rename 方法 windows 平台会有权限问题
+      // https://github.com/meteor/meteor/issues/7852#issuecomment-255767835
+      await fse.move(chunk.path, `${chunkDir}/${hash}`);
+      res.status = 200;
+      res.end(
+        JSON.stringify({
+          code: 200,
+          message: "received file chunk",
+        })
+      );
+    });
+  }
+
+  if (req.url === "/merge") {
+    const data = await resolvePost(req);
+    const { filename, fileHash } = data;
+    const filePath = `${UPLOAD_DIR}/${filename}`;
+    await mergeFileChunk(filePath, fileHash);
+    res.end(
+      JSON.stringify({
+        code: 200,
+        message: "file merged success",
+      })
+    );
+  }
 });
 
 server.listen(3000, () => console.log("正在监听 3000 端口"));
