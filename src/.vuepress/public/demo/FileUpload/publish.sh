8bbf02009feb3a56ca86f841317b712a4ad71447
songxingguo <xg.song@qq.com>
2024-06-16 16:51:45 +0800

    feat:  提取submodule demo

--- src/.vuepress/public/demo/FileUpload/publish.sh
+++
@@ -1,3 +0,0 @@
-docker login --username=18983642737 registry.cn-shanghai.aliyuncs.com
-docker build . --file Dockerfile --tag registry.cn-shanghai.aliyuncs.com/songxingguo/bigfileupload:latest
-docker push registry.cn-shanghai.aliyuncs.com/songxingguo/bigfileupload:latest
